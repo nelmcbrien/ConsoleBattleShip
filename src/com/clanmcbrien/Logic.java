6aa24fa24ae42ef726df27b2cbd04470e737d7fd
nelmcbrien <nelmcbrien@gmail.com>
2014-08-28 09:35:31 -0500

    Merge

--- src/com/clanmcbrien/Logic.java
+++ src/com/clanmcbrien/Logic.java
@@ -24,7 +24,7 @@ public class Logic {
     public static void populateBoard(String [][] board){
         for(int i=0; i < 5; i++){
             for (int j=0; j < 5; j++){
-                board[j][i] = "O ";
+                board[j][i] = ".";
             }
         }
     }    
